ad3b9f860eeb047a5e477c682d4e59b7d77cf0dd
boxiu.li <boxiu.li@bridgedp.com>
2026-06-11 02:08:39 +0800

    feat: 赋值操作约束上下文描述
    添加赋值操作的类型不匹配上下文描述:
    - 变量赋值: '赋值类型不匹配'
    - 字段赋值: '字段赋值类型不匹配'
    - 数组元素赋值: '数组元素赋值类型不匹配'

--- karte-hir/src/type_checker.rs
+++ karte-hir/src/type_checker.rs
@@ -2854,20 +2854,20 @@ impl TypeChecker {
                 match &**target {
                     Expr::Identifier { .. } => {
                         // 变量赋值：统一类型
-                        self.add_constraint(target_type, value_type, *span);
+                        self.add_constraint_with_context(target_type, value_type, *span, "赋值类型不匹配");
                     }
                     Expr::Constructor { args, .. } if args.is_empty() => {
                         // 大写字母开头的变量被 parser 误解析为零参数 Constructor
                         // 在赋值目标位置应视为普通变量
-                        self.add_constraint(target_type, value_type, *span);
+                        self.add_constraint_with_context(target_type, value_type, *span, "赋值类型不匹配");
                     }
                     Expr::FieldAccess { .. } => {
                         // 字段赋值：统一类型
-                        self.add_constraint(target_type, value_type, *span);
+                        self.add_constraint_with_context(target_type, value_type, *span, "字段赋值类型不匹配");
                     }
                     Expr::Index { .. } => {
                         // 数组下标赋值：统一类型
-                        self.add_constraint(target_type, value_type, *span);
+                        self.add_constraint_with_context(target_type, value_type, *span, "数组元素赋值类型不匹配");
                     }
                     _ => {
                         // 其他表达式不能作为赋值目标
